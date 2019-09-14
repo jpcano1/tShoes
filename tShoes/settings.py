85bfce9e0f5404851117125fcf3e134e247bcca7
Ubuntu <ubuntu@ip-172-31-22-186.ec2.internal>
2019-09-14 20:33:25 +0000

    Allowed Hosts

--- tShoes/settings.py
+++ tShoes/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'hcq&(jt)gu1p12__y*n_j012c4zi*ffq8$s_&n8oyfcdhu@k8m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.0.10", "0.0.0.0"]
+ALLOWED_HOSTS = [
+        "localhost", 
+        "127.0.0.1", 
+        "192.168.0.10", 
+        "0.0.0.0",
+        "3.88.140.100"
+        ]
 
 # Users and authentication
 AUTH_USER_MODEL = 'users.User'
@@ -127,7 +133,8 @@ CORS_ORIGIN_WHITELIST = (
     "http://localhost:4200",
     "http://127.0.0.1:8080",
     "http://192.168.0.10:8080",
-    "https://localhost:8080"
+    "https://localhost:8080",
+    "http://3.88.140.100:8000"
 )
 
 # Internationalization
