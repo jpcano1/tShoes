40c7068baa8f32c570de5bc2babaaf3bc4c6bb2c
JUAN PABLO CANO BUITRAGO <jp.cano@sis.virtual.uniandes.edu.co>
2019-09-16 08:51:01 -0500

    Arreglo de modelos

--- bill/models/bill.py
+++ bill/models/bill.py
@@ -8,3 +8,7 @@ class Bill(TShoesModel, models.Model):
     """ Class that represents the bill of the order """
 
     order = models.OneToOneField('order.Order', on_delete=models.CASCADE)
+
+    def __str__(self):
+        """ toString function """
+        return "id of the bill: {}".format(str(self.pk))
