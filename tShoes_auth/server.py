181b27887624a544ee7c1451101e7493dd98f4cf
Juan Pablo Cano <jp.cano@uniandes.edu.co>
2019-11-04 12:25:40 -0500

    Nuevo Puerto

--- tShoes_auth/server.py
+++ tShoes_auth/server.py
@@ -121,5 +121,5 @@ if __name__ == '__main__':
     #                               password=POSTGRES_PASSWORD)
     # cur = connection.cursor()
     token_conn = Authtoken()
-    app.run(debug=True, host='0.0.0.0', port=5555)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
